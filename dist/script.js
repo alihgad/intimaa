9ff589a0a51cbc7d031650d2cadfaef31f684b8c
AhmedSaadallah1 <Ahmedmohamed19982@yahoo.com>
2025-06-04 13:07:53 +0300

    last

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,5 @@
+// toggle button navbar
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("menu-toggle");
   const menu = document.getElementById("mobile-menu");
@@ -7,21 +9,56 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-const navLinks = document.querySelectorAll("nav .lg\\:flex a");
+// change top header when scroll
+
+const topBar = document.querySelector(".bg-transparent.mt-2");
+const topBarText = topBar.querySelectorAll("span");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
-    navLinks.forEach((link) => {
-      link.classList.add("text-blue-800");
+    topBarText.forEach((el) => {
+      el.classList.remove(
+        "text-white",
+        "text-white/80",
+        "text-white/60",
+        "text-white/40",
+        "text-gray-300"
+      );
+      el.classList.add("text-blue-800");
     });
   } else {
-    navLinks.forEach((link) => {
-      link.classList.remove("text-blue-800");
+    topBarText.forEach((el) => {
+      el.classList.remove("text-blue-800");
+      if (el.textContent === "|") {
+        el.classList.add("text-white/40");
+      } else if (
+        el.textContent.includes("+966") ||
+        el.textContent.includes("تواصل")
+      ) {
+        el.classList.add("text-gray-300");
+      } else if (el.textContent === "EN") {
+        el.classList.add("text-white/60");
+      } else {
+        el.classList.add("text-white/80");
+      }
     });
   }
 });
 
-// Declare currentSlide once at the top
+// change colors on scroll navbar
+
+const nav = document.querySelector("nav"); // Select the nav element
+const navLinks = document.querySelectorAll("nav .lg\\:flex a"); // Select nav links
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    nav.classList.add("bg-blue-800", "rounded-lg"); // Add background and border radius
+  } else {
+    nav.classList.remove("bg-blue-800", "rounded-lg"); // Remove on scroll up
+  }
+});
+
+//landing page slider
 let currentSlide = 0;
 const slider = document.getElementById("slider");
 const dots = document.querySelectorAll(".dot");
